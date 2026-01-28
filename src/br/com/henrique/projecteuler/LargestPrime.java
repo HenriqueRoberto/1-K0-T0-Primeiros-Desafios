432cd0c46d4c4e231d66ccb10718e485b91373f0
HenriqueRoberto <contatohenrique.hrs@gmail.com>
2026-01-28 19:20:24 -0300

    exercicio do maior fator primo

--- src/br/com/henrique/projecteuler/LargestPrime.java
+++ src/br/com/henrique/projecteuler/LargestPrime.java
@@ -1,4 +1,18 @@
+//The prime factors of 13195 are 5, 7, 13 and 29.
+//
+//What is the largest prime factor of the number 600851475143?
 package br.com.henrique.projecteuler;
 
 public class LargestPrime {
+  public void execute(){
+    long largeFactor = 0;
+    long n  = 600851475143L;
+    for (long i = 2; i <= n ; i++ ){
+      while (n % i == 0){
+        largeFactor = i;
+        n = n/i;
+      }
+    }
+    System.out.println(largeFactor);
+  }
 }
