842935ec6587d64d1f571548034a37c85320fc2d
HenriqueRoberto <contatohenrique.hrs@gmail.com>
2026-01-28 18:43:14 -0300

    feat: Menu de execução da main

--- src/br/com/henrique/projecteuler/Main.java
+++ src/br/com/henrique/projecteuler/Main.java
@@ -2,7 +2,18 @@ package br.com.henrique.projecteuler;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        while (true){
+            System.out.println("===== Menu =====");
+            System.out.println("1 - Multiple of 3 or 5");
+            System.out.println("2 - Even Fibonacci Numbers");
+            System.out.println("3 - Largest Prime Factor");
+            System.out.println("0 - Exit");
+
+            break;
+
+
+        }
+
 
     }
 }
